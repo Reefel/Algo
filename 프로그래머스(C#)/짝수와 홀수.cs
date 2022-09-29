a38c6de7b5f7a277bca240242e80ad077047cdc0
LMGi99830 <96801658+LMGi99830@users.noreply.github.com>
2022-09-29 09:23:05 +0900

    Create 짝수와 홀수.cs

---
+++ 프로그래머스(C#)/짝수와 홀수.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public string solution(int num) {
+        string answer = "";
+        if(num % 2 == 0) return answer = "Even";
+        return answer = "Odd";
+    }
+}
